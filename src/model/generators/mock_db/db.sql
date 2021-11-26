aa6e9534674d7871ea2901edf2a8e49ddbe31121
dmbg1 <dmbg1999@hotmail.com>
2021-11-26 15:57:45 +0000

    Mock db almost finished

--- src/model/generators/mock_db/db.sql
+++ src/model/generators/mock_db/db.sql
@@ -1,34 +1,55 @@
+CREATE DATABASE IF NOT EXISTS test;
+
+USE test;
+
+DROP TABLE IF EXISTS rack_piece;
+DROP TABLE IF EXISTS piece;
+DROP TABLE IF EXISTS rack;
+DROP TABLE IF EXISTS shelf;
+DROP TABLE IF EXISTS warehouse;
+
 create table warehouse (
-    id INT PRIMARY KEY
+    id SERIAL,
+    CONSTRAINT TablePK PRIMARY KEY(id)
 );
 
 create table shelf (
-    id INT PRIMARY KEY,
-    warehouse_id INT NOT NULL REFERENCES warehouse(id), 
-    x NOT NULL INT,
-    y NOT NULL INT
+    id SERIAL,
+    warehouse_id BIGINT UNSIGNED NOT NULL, 
+    x INT NOT NULL,
+    y INT NOT NULL,
+    CONSTRAINT TablePK PRIMARY KEY(id),
+    CONSTRAINT warehouse_fk FOREIGN KEY (warehouse_id) REFERENCES warehouse(id)
 );
 
 create table rack (
-    id INT PRIMARY KEY, 
-    shelf_id INT NOT NULL REFERENCES shelf(id),
-    y NOT NULL DECIMAL(9,2),
-    length NOT NULL INT,
-    width NOT NULL DECIMAL(9,2), 
-    height NOT NULL DECIMAL(9,2),
-    capacity NOT NULL DECIMAL(9,2) 
+    id SERIAL, 
+    shelf_id BIGINT UNSIGNED NOT NULL,
+    y DECIMAL(9,2) NOT NULL,
+    length INT NOT NULL,
+    width DECIMAL(9,2) NOT NULL, 
+    height DECIMAL(9,2) NOT NULL,
+    capacity DECIMAL(9,2) NOT NULL,
+    CONSTRAINT TablePK PRIMARY KEY(id),
+    CONSTRAINT shelf_fk FOREIGN KEY (shelf_id) REFERENCES shelf(id)
+
+
 );
 
 create table piece (
-    id INT PRIMARY KEY, 
-    width NOT NULL DECIMAL(9,2), 
-    height NOT NULL DECIMAL(9,2),
-    weight NOT NULL DECIMAL(6,2)
+    id SERIAL, 
+    width DECIMAL(9,2) NOT NULL, 
+    height DECIMAL(9,2) NOT NULL,
+    weight DECIMAL(6,2) NOT NULL,
+    CONSTRAINT TablePK PRIMARY KEY(id)
 );
 
 create table rack_piece (
-    x_orig NOT NULL INT,
-    x_end NOT NULL INT,
-    rack_id INT NOT NULL REFERENCES rack(id),
-    piece_id INT NOT NULL REFERENCES piece(id)
+    x_orig INT NOT NULL,
+    x_end INT NOT NULL,
+    rack_id BIGINT UNSIGNED NOT NULL,
+    piece_id BIGINT UNSIGNED NOT NULL,
+    CONSTRAINT TablePK PRIMARY KEY(rack_id, piece_id),
+    CONSTRAINT rack_fk FOREIGN KEY (rack_id) REFERENCES rack(id),
+    CONSTRAINT piece_fk FOREIGN KEY (piece_id) REFERENCES piece(id)
 );
--- src/model/generators/mock_db/warehouse_mock.sql
+++ src/model/generators/mock_db/warehouse_mock.sql
@@ -1,3 +1,5 @@
+USE test;
+
 insert into warehouse (id) values (1);
 
 insert into shelf (warehouse_id, x, y) values (1, 4, 0);
@@ -62,81 +64,81 @@ insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.0, 1.5, 1, 1)
 insert into piece (width, height, weight) values (1.6, 0.95, 120);
 insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.5, 4, 1, 2);
 insert into piece (width, height, weight) values (1, 0.5, 25);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(4, 4.5, 1, 4);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(4, 4.5, 1, 3);
 -- rack 2
 insert into piece (width, height, weight) values (1.35, 0.7, 200);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.0, 3, 2, 5);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.0, 3, 2, 4);
 -- rack 3
 insert into piece (width, height, weight) values (1.15, 0.81, 196);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.5, 3, 3, 6);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.5, 3, 3, 5);
 insert into piece (width, height, weight) values (0.78, 0.63, 259);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.0, 1,5, 3, 7);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.0, 1.5, 3, 6);
 -- rack 4
 insert into piece (width, height, weight) values (0.99, 1.35, 445);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0, 2.5, 4, 8);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0, 2.5, 4, 7);
 insert into piece (width, height, weight) values (0.53, 0.5, 282);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3, 4, 4, 9);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3, 4, 4, 8);
 insert into piece (width, height, weight) values (0.3, 0.87, 65);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(4.0, 4.5, 4, 10);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(4.0, 4.5, 4, 9);
 -- rack 5
 insert into piece (width, height, weight) values (0.47, 0.39, 104);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.5, 2.7, 5, 11);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.5, 2.7, 5, 10);
 insert into piece (width, height, weight) values (0.23, 0.43, 514);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0, 1.25, 5, 12);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0, 1.25, 5, 11);
 -- rack 6
 insert into piece (width, height, weight) values (0.87, 0.97, 12);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.5, 2.3, 6, 13);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.5, 2.3, 6, 12);
 insert into piece (width, height, weight) values (0.46, 0.53, 309);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.3, 1.4, 6, 14);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.3, 1.4, 6, 13);
 insert into piece (width, height, weight) values (0.54, 0.51, 310);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3.4, 4.5, 6, 15);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3.4, 4.5, 6, 14);
 -- rack 7
 insert into piece (width, height, weight) values (0.96, 0.53, 270);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.5, 3.0, 7, 16);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.5, 3.0, 7, 15);
 insert into piece (width, height, weight) values (0.84, 0.36, 4);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.4, 1.7, 7, 17);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.4, 1.7, 7, 16);
 -- rack 8
 insert into piece (width, height, weight) values (1.65, 0.46, 155);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3.6, 4.7, 8, 18);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3.6, 4.7, 8, 17);
 insert into piece (width, height, weight) values (0.86, 0.57, 106);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.5, 3.0, 8, 19);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.5, 3.0, 8, 18);
 insert into piece (width, height, weight) values (1.72, 0.74, 166);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.4, 1.5, 8, 20);
---rack 9
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.4, 1.5, 8, 19);
+-- rack 9
 insert into piece (width, height, weight) values (1.86, 0.28, 335);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.4, 1.5, 9, 21);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.4, 1.5, 9, 20);
 insert into piece (width, height, weight) values (0.28, 0.49, 449);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.7, 3, 9, 22);
---rack 10
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.7, 3, 9, 21);
+-- rack 10
 insert into piece (width, height, weight) values (0.4, 0.73, 160);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.4, 2.4, 10, 23);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0.4, 2.4, 10, 22);
 insert into piece (width, height, weight) values (0.8, 0.21, 224);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3.5, 5.7, 10, 24);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3.5, 5.7, 10, 23);
 insert into piece (width, height, weight) values (0.51, 0.3, 95);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(6, 7, 10, 25);
---rack 11
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(6, 7, 10, 24);
+-- rack 11
 insert into piece (width, height, weight) values (0.61, 1.05, 609);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0, 2.4, 11, 26);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(0, 2.4, 11, 25);
 insert into piece (width, height, weight) values (0.58, 0.38, 29);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.8, 3.3, 11, 27);
---rack 12
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.8, 3.3, 11, 26);
+-- rack 12
 insert into piece (width, height, weight) values (0.9, 0.19, 303);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.8, 3.9, 12, 28);
---rack 13
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.8, 3.9, 12, 27);
+-- rack 13
 insert into piece (width, height, weight) values (0.5, 1.24, 103);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.2, 2, 13, 29);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1.2, 2, 13, 28);
 insert into piece (width, height, weight) values (0.34, 0.49, 264);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.6, 3.5, 13, 30);
---rack 14
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.6, 3.5, 13, 29);
+-- rack 14
 insert into piece (width, height, weight) values (1.4, 0.55, 330);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(4, 4.9, 14, 31);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(4, 4.9, 14, 30);
 insert into piece (width, height, weight) values (2.25, 0.63, 70);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.6, 3.5, 14, 32);
---rack 15
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(2.6, 3.5, 14, 31);
+-- rack 15
 insert into piece (width, height, weight) values (1.36, 0.63, 486);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1, 2.9, 15, 33);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(1, 2.9, 15, 32);
 insert into piece (width, height, weight) values (1.73, 0.38, 139);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(4, 4.7, 15, 34);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(4, 4.7, 15, 33);
 insert into piece (width, height, weight) values (1.42, 0.64, 171);
-insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3.4, 4, 15, 35);
+insert into rack_piece (x_orig, x_end, rack_id, piece_id) values(3.4, 4, 15, 34);
 
