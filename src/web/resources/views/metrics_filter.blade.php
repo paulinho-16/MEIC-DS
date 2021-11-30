92de6188ea61a73cfade0df6e47435f143ca747a
up201806723 <up201806723@up.pt>
2021-11-30 23:40:45 +0000

    starting filters page

---
+++ src/web/resources/views/metrics_filter.blade.php
@@ -0,0 +1,15 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+                <button type="button" class="btn btn-outline-info">Info</button>
+                <button type="button" class="btn btn-outline-info">Info</button>
+                <button type="button" class="btn btn-outline-info">Info</button>
+                <button type="button" class="btn btn-outline-info">Info</button>
+        </div>
+    </div>
+</div>
+@endsection
--- src/web/routes/web.php
+++ src/web/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/filters', function () {
+    return view('metrics_filter');
+});
