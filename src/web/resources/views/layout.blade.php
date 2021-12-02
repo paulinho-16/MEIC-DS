3b715a349f34021643acef3deb3aac759ad01ceb
jessymireie <up201806723@up.pt>
2021-12-02 23:20:49 +0000

    added menu to choose the row and visualize and export file buttons to the layout screen

--- src/web/resources/views/layout.blade.php
+++ src/web/resources/views/layout.blade.php
@@ -1,6 +1,29 @@
 @extends('app')
 @section('content')
 <div class="container">
+    <h1 class="d-flex justify-content-center mt-5">Visualize Warehouse</h1>
+    <div class="container">
+        <div class="row">
+            <form class="d-flex">
+                <!--add form to get which row it wants-->
+                <h5 class="mt-1">Row</h5>
+                <select class="custom-select  ml-2">
+                    <option selected>Choose row</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+                <button type="submit" class="ml-2 btn btn-outline-info "> Visualize</button>
+            </form>
+            
+            <!--add an export action-->
+            <div class="ml-auto">
+                <button type="button" class="btn btn-outline-info ">Export File</button>
+            </div>  
+            
+        </div>
+    </div>
+
     <div id="carouselContent" class="carousel slide mt-4" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
 
@@ -56,11 +79,11 @@
 
         </div>
 
-       
+        <!--Controls-->
 
         <a class="carousel-control-prev bg-info" href="#carouselContent" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only text-primary">Previous</span>
+            <span class="carousel-control-prev-icon text-primary" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
         </a>
         <a class="carousel-control-next bg-info" href="#carouselContent" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
