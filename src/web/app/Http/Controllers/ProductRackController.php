283abcf52761560446b055bc333bf4e11997e202
jessymireie <up201806723@up.pt>
2021-12-24 00:04:43 +0000

    layout with some info from db

--- src/web/app/Http/Controllers/ProductRackController.php
+++ src/web/app/Http/Controllers/ProductRackController.php
@@ -6,14 +6,56 @@ use Illuminate\Http\Request;
 
 use App\Models\Product_Rack;
 use App\Models\Product;
+use App\Models\Rack;
+use App\Models\Shelf;
 
 class ProductRackController extends Controller
 {
     public function show()
     {
+        $products_rack=Product_Rack::orderby('rack_id')->get();
+
+        $shelf_list=array();
+        $shelf_ids=array();
+        foreach(Shelf::all() as $shelf){
+            $racks_list=array();
+            foreach(Rack::all() as $rack){
+                if($shelf->id == $rack->shelf_id){
+                    $racks_list[]=$rack;
+                }
+            }
+            $shelf_list[]=$racks_list;
+            $shelf_ids[]=$shelf->id;
+        }
+        
+
         return view('layout', [
-            'product_racks' => Product_Rack::all(),
-            'products' => Product::all()
+           'products_rack'=> $products_rack,
+           'shelf_list'=> $shelf_list,
+           'shelf_ids'=> $shelf_ids,
         ]);
+    }
+
+    public function orderRacks($shelf_id,$shelf_list){
+        $rackToOrder=array();
+        foreach($shelf_list as $shelf){
+            foreach($shelf as $rack){
+                if($rack->shelf_id == $shelf_id)
+                    $rackToOrder[]=$rack;
+            }
+        }
+        return collect($rackToOrder)->sortBy('y')->reverse();
+    }
+
+    public function orderProducts($rack_id,$products_rack){
+
+        $productsToOrder=array();
+        foreach($products_rack as $product){
+            if($product->rack_id == $rack_id){
+                $productsToOrder[]=$product;
+            }
+        }
+        //echo collect($productsToOrder)->sortBy('x_orig'), ' next: ';
+        return collect($productsToOrder)->sortBy('x_orig') ;
     }
 }
--- src/web/resources/views/layout.blade.php
+++ src/web/resources/views/layout.blade.php
@@ -2,80 +2,78 @@
 @section('content')
 <div class="container">
     <h1 class="d-flex justify-content-center mt-5">Visualize Warehouse</h1>
-    <div class="container">
-        <div class="row">
-            <form class="d-flex">
-                <!--add form to get which row it wants-->
-                <h5 class="mt-1">Row</h5>
-                <select class="custom-select  ml-2">
-                    <option selected>Choose row</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
-                <button type="submit" class="ml-2 btn btn-outline-info "> Visualize</button>
-            </form>
-            
-            <!--add an export action-->
-            <div class="ml-auto">
-                <button type="button" class="btn btn-outline-info ">Export File</button>
-            </div>  
-            
-        </div>
-    </div>
-
     <div id="carouselContent" class="carousel slide mt-4" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
+            
+            <?php
 
-            <!--First Slide-->
-            <div class="carousel-item active text-center p-4">
-                <div class="row d-flex justify-content-center">
-                    @for ($i = 0; $i < 3; $i++)
-                    <div class="m-3">
-                        <table class="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th><h2> {{$i}}</h2></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($product_racks as $product_rack)
-                                <tr>
-                                    <td>{{$product_rack}}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    @endfor
-                </div>
-            </div>
+use App\Http\Controllers\ProductRackController;
+use App\Models\Product;
 
+echo '<div class="carousel-item active text-center p-4">
+                <div class="row d-flex justify-content-center">';
+                    $i=1;
+                    foreach($shelf_ids as $shelf_id){ 
+                        if($i==$shelf_id && $i<=6){
+                            echo '<div class="m-3">
+                                <table class="table table-striped table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th><h2>',$shelf_id,'</h2></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>';
 
-            <!--Second Slide-->
-            <div class="carousel-item text-center p-4">
-                <div class="row d-flex justify-content-center">
-                    @for ($i = 3; $i < 6; $i++)
-                    <div class="m-3">
-                        <table class="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th><h2> {{$i}}</h2></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @for ($j = 0; $j < 5; $j++)
-                                <tr>
-                                    <td>Parafusos 20</td>
-                                </tr>
-                                @endfor
-                            </tbody>
-                        </table>
-                    </div>
-                    @endfor
-                </div>
-            </div>
+                                    foreach(ProductRackController::orderRacks($shelf_id,$shelf_list) as $rack){
+                                        echo '<tr>';
+                                        foreach(ProductRackController::orderProducts($rack->id,$products_rack) as $product_rack) {
+                                            echo '<td>', Product::where('id',$product_rack->product_id)->first()->id,'</td>';
+                                        }
+                                        echo '</tr>';
+                                    }
+                                    echo '</tbody>
+                                </table>
+                            </div>';
+                        }
+                        $i++;
+                    }
+                echo'</div>
+            </div>';
 
+            $k=7;
+            for($j=2; $j <= count($shelf_ids)/6; $j++){
+            echo '<div class="carousel-item text-center p-4">
+                <div class="row d-flex justify-content-center">';
+                   
+                    foreach($shelf_ids as $shelf_id){ 
+                        if($k==$shelf_id && $k<=6*$j){
+                            echo '<div class="m-3">
+                                <table class="table table-striped table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th><h2> ',$shelf_id,'</h2></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>';
+                                        foreach(ProductRackController::orderRacks($shelf_id,$shelf_list) as $rack){
+                                            echo '<tr>';
+                                            foreach(ProductRackController::orderProducts($rack->id,$products_rack) as $product_rack) {
+                                                echo '<td>', Product::where('id',$product_rack->product_id)->first()->id,'</td>';
+                                            }
+                                            echo '</tr>';
+                                        }
+                                    echo '</tbody>
+                                </table>
+                            </div>';
+                            $k++;
+                        }
+
+                    };
+                    $k=6*$j+1;
+                echo' </div>
+            </div>';
+            };
+        ?>            
 
         </div>
 
