30de98d0e19b911cf5ef890885d2f0c29b2f973e
jessymireie <up201806723@up.pt>
2022-01-10 17:30:09 +0000

    racks and cells with more info

---
+++ src/web/app/Models/Product_Type.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product_Type extends Model
+{
+    protected $table = 'Product_Type';
+
+    public $timestamps = false;
+
+}
--- src/web/resources/views/partials/show_results/table.blade.php
+++ src/web/resources/views/partials/show_results/table.blade.php
@@ -3,19 +3,33 @@
     <tr class="text-center font-weight-bold">
         <td colspan="{{$sizes[$shelf->id]}}">{{$shelf->id}}</td>
     </tr>
+    <tr class="text-center">
+        <td class="align-middle font-weight-bold">Racks</td>
+        <td colspan="{{$sizes[$shelf->id]-1}}" class="align-middle font-weight-bold">Products</td>
+    </tr>
     </thead>
+    
     @foreach($shelf->racks->sortByDesc('y')->values() as $rack)
         <tr class="text-center">
-            <td class="align-middle font-weight-bold">{{$rack->id}}</td>
+            <td class="align-middle font-weight-bold">
+                <h5>{{$rack->id}}</h5>
+                <p>Y: {{ (($int = (int)$rack->y) == $rack->y) ? $int : $rack->y;}}</p>
+                <p>Capacity: {{(($int = (int)$rack->capacity) == $rack->capacity) ? $int : $rack->capacity;}}</p>
+            </td>
             @foreach($results[$rack->id] as $product_racks)
                 @if(count($product_racks)===0)
                     <td colspan="{{$sizes[$shelf->id]}}">Sem Resultados</td>
                 @endif
                 @foreach($product_racks as $product_rack)
                     <td class="align-middle">
-                        <p>{{$product_rack->product->id}}</p>
+                        <h5>{{$product_rack->product->id}}</h5>
                         @if($product_rack->product->name)
-                            <p>{{$product_rack->product->name}}</p>
+                            <p>Name:{{$product_rack->product->name}}</p>
+                        @endif
+                        <p>Weight: {{(($int = (int)$product_rack->product->weight) == $product_rack->product->weight) ? $int : $product_rack->product->weight;}}</p>
+                        <p>Type: {{$product_rack->product->type_id}}</p>
+                        @if($product_rack->product->frequency != 0)
+                            <p>Frequency: {{(($int = (int)$product_rack->product->frequency) == $product_rack->product->frequency) ? $int : $product_rack->product->frequency;}}</p>
                         @endif
                     </td>
                 @endforeach
