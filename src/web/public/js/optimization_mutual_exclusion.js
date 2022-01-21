378d5a71b7d14cd146210bd6d425e96494869032
Ruben Almeida <almeidaruben35@gmail.com>
2022-01-21 20:01:23 +0000

    Mutual exclusion hide also the select

--- src/web/public/js/optimization_mutual_exclusion.js
+++ src/web/public/js/optimization_mutual_exclusion.js
@@ -7,6 +7,15 @@
 
         if (workInput.checked) {
             $("#alert-mutual-exclusion").show()
+
+            const col_elem = document.querySelector('#col-work')
+
+            const input_elem = col_elem.querySelector('select')
+
+            col_elem.classList.remove('d-block')
+            col_elem.classList.add('d-none')
+
+            input_elem.value = null
             return workInput.checked = false
         }
 
@@ -16,16 +25,40 @@
 
         if (weightInput.checked) {
             $("#alert-mutual-exclusion").show()
+
+            const col_elem = document.querySelector('#col-weight')
+
+            const input_elem = col_elem.querySelector('select')
+
+            col_elem.classList.remove('d-block')
+            col_elem.classList.add('d-none')
+
+            input_elem.value = null
+
             return weightInput.checked = false
         }
+    }
+
+    const handleMinimizeErrorsSelect = (event) => {
+        event.preventDefault()
+
+    }
+    const handleTypeSelect = (event) => {
+        event.preventDefault()
+
     }
 
     const weightInput = document.querySelector('#weight')
     const workInput = document.querySelector('#work')
 
+    const minimizeErrors = document.querySelector('#minimize-errors')
+    const typeMetric = document.querySelector('#organization')
+
     weightInput.addEventListener('change', handleWeightSelect)
     workInput.addEventListener('change', handleWorkSelect)
-    
+    minimizeErrors.addEventListener('change', handleMinimizeErrorsSelect)
+    typeMetric.addEventListener('change', handleMinimizeErrorsSelect)
+
 })()
 
 
